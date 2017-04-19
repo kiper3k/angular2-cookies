43220d4bd4a8de3a84537b151c8666d77edb6346
kiper3k <kiper3k@gmail.com>
2017-04-19 02:18:54 +0200

    ciastka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cookie } from 'ng2-cookies'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular2 Cookies';
+  cookies: Object;
+  keys: Array<string>;
+  addName: string;
+  addValue: string;
+  deleteName: string;
+
+  constructor() {
+    this.update();
+  }
+
+  update() {
+    this.cookies = Cookie.getAll();
+    this.keys = Object.keys(this.cookies);
+  }
+
+  addCookie(addName: string, addValue: string) {
+    Cookie.set(addName, addValue);
+    this.update();
+  }
+
+  deleteCookie(deleteName: string) {
+    Cookie.delete(deleteName);
+    this.update();
+  }
 }
